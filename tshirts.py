c6b8256610e9280e2686c5dc05204297dca13b42
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-08 12:23:21 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -10,7 +10,9 @@ def size(cms):
 
 assert(size(37) == 'S')
 assert(size(38) == 'S')
+assert(size(39) == 'M')
 assert(size(40) == 'M')
+assert(size(41) == 'M')
 assert(size(42) == 'L')
 assert(size(43) == 'L')
 print("All is well (maybe!)\n")
