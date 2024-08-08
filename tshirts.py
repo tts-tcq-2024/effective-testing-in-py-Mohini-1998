394c1f93be63a9714de100d42d7b2d741fb9f943
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-08 11:48:03 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -4,11 +4,11 @@ def size(cms):
         return 'S'
     elif cms > 38 and cms < 42:
         return 'M'
-    #else:
-       # return 'L'
+    else:
+        return 'L'
 
 
 assert(size(37) == 'S')
 assert(size(40) == 'M')
-#assert(size(43) == 'L')
+assert(size(43) == 'L')
 print("All is well (maybe!)\n")
