e0c2b392127284fab0a29003a5bb92292d6b8713
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-08 11:39:32 +0530

    Update tshirt_test.py

--- tshirt_test.py
+++ tshirt_test.py
@@ -1,1 +1,29 @@
-def
+import unittest
+from tshirt import size
+class Testtshirtsize(unittest.TestCase):
+        
+        def test_expectZeroForEmptyInput(self):
+                self.assertEqual(size(37)== 'S')
+                
+        #def expectZeroForSingleZero(self):
+          #      self.assertEqual(add("0"), 0)
+                
+       # def expectSumForTwoNumberst(self):
+             #   self.assertEqual(add("1,2"), 3)
+                
+        #def ignoreNumbersGreaterThan1000(self):
+          #      self.assertEqual(add("1,1001"), 1)
+                
+       # def expectSumWithCustomDelimiter(self):
+          #      self.assertEqual(add("//;\n1;2"), 3)
+                
+        #def expectSumWithNewlineDelimiter(self):
+            #    self.assertEqual(add("1\n2,3"),6);
+        
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
